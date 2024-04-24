e28d70f653ec9fefb8748bfadcabf216e0903254
Madhav reddy <reddydharani59@gmail.com>
2024-04-24 14:48:47 +0530

    using common scripts

---
+++ db.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+source ./common.sh
+CHECK_ROOT
+
+echo "Please enter DB password:"
+read -s mysql_root_password
+
+dnf install mysql-server -y &>>$LOGFILE
+VALIDATE $? "Installing MySQL Server"
+
+systemctl enable mysqld &>>$LOGFILE
+VALIDATE $? "Enabling MySQL Server"
+
+systemctl start mysqld &>>$LOGFILE
+VALIDATE $? "Starting MySQL Server"
+
+# mysql_secure_installation --set-root-pass ExpenseApp@1 &>>$LOGFILE
+# VALIDATE $? "Setting up root password"
+
+#Below code will be useful for idempotent nature
+mysql -h db.daws-78s.site -uroot -p${mysql_root_password} -e 'show databases;' &>>$LOGFILE
+if [ $? -ne 0 ]
+then
+    mysql_secure_installation --set-root-pass ${mysql_root_password} &>>$LOGFILE
+    VALIDATE $? "MySQL Root password Setup"
+else
+    echo -e "MySQL Root password is already setup...$Y SKIPPING $N"
+fi
