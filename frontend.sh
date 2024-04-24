ab716e921f74b4607c4f61043855120688c94b44
Madhav reddy <reddydharani59@gmail.com>
2024-04-24 16:09:15 +0530

    using common scripts

--- frontend.sh
+++ frontend.sh
@@ -21,7 +21,7 @@ unzip /tmp/frontend.zip &>>$LOGFILE
 VALIDATE $? "Extracting frontend code"
 
 #check your repo and path
-cp /home/ec2-user/expense-project/nginxcong.sh /etc/nginx/default.d/expense.conf &>>$LOGFILE
+cp /home/ec2-user/expense-another/nginxcong.sh /etc/nginx/default.d/expense.conf &>>$LOGFILE
 VALIDATE $? "Copied expense conf"
 
 systemctl restart nginx &>>$LOGFILE
