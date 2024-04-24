761bacf4fdd6015ad9df89519d8990a67e703ae0
Madhav reddy <reddydharani59@gmail.com>
2024-04-24 17:07:18 +0530

    using common scripts

--- common.sh
+++ common.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 set -e
 handle_error(){
-    echo "error occurred at line number: $1, error occured at $2"
+    echo "Error occured at line number: $1, error command: $2"
 }
-trap handle_error ${LINENO} "$BASH_COMMAND" ERR
+
+trap 'handle_error ${LINENO} "$BASH_COMMAND"' ERR
+
 USERID=$(id -u)
 TIMESTAMP=$(date +%F-%H-%M-%S)
 SCRIPT_NAME=$(echo $0 | cut -d "." -f1)
